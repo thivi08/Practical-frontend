5f0fd57b63b08678232b7dee2e9ab6291a7f887d
Thiviya Thuraisingham <thiviya.thurai@kochasoft.lk>
2022-06-17 14:04:10 +0530

    function added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { CarouselService } from 'src/service/carousel.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-count: number = 5
+  count: number = 5
   sliderList: any[] = [];
 
   constructor(private carouselService: CarouselService) { }
@@ -18,33 +18,46 @@ count: number = 5
 
   }
 
-  getSliderList(){
-      this.carouselService.getSliderList(this.count)
-        .subscribe({
-          next: (response: any | null | undefined) => {
-            try {  
-              if (!response) {
-                return;
-              }  
-              this.sliderList = response.slides;
-              console.log(">>>>>>>>>", this.sliderList)
-  
-            } catch (error) {
-              console.log("Error", error);
+  getSliderList() {
+    this.carouselService.getSliderList(this.count)
+      .subscribe({
+        next: (response: any | null | undefined) => {
+          try {
+            if (!response) {
+              return;
             }
-          },
-          error: (error) => {
-            console.log('Error in get list', error)
+            this.sliderList = response.slides;
+            console.log(">>>>>>>>>", this.sliderList)
+
+          } catch (error) {
+            console.log("Error", error);
           }
-        })
-    
-  }
-
-  next(){
-
-  }
-
-  prev(){
+        },
+        error: (error) => {
+          console.log('Error in get list', error)
+        }
+      })
 
   }
+  slides: string [] = ['./assets/SBI_Slide_1.jpg', './assets/Eagle_Slide_2.jpg', './assets/Knot_Slide_3.jpg' ]
+  i=0;
+
+  getSlide() {
+      return this.sliderList[this.i].image;
+  }
+  getTitle() {
+      return this.sliderList[this.i].title;
+  }
+  getSubTitle() {
+      return this.sliderList[this.i].subTitle;
+  }
+
+  getPrev() {
+      this.i = this.i===0 ? 0 : this.i - 1;
+  }
+//edit here    
+  getNext() {
+      this.i = this.i===this.sliderList.length ? this.i : this.i + 1;
+  }
+
 }
